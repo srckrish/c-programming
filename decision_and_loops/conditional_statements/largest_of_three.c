92d10ab59f0c9a3d1eb8585035eadb8bb876ab99
srckrish <krish.dahal82@gmail.com>
2025-12-29 22:01:39 +0545

    structure folder name

---
+++ decision_and_loops/conditional_statements/largest_of_three.c
@@ -0,0 +1,23 @@
+// WAP to find the largest of three numbers
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter any three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("%d is the greatest number.\n", a);
+    } 
+    else if (b >= a && b >= c) {
+        printf("%d is the greatest number.\n", b);
+    } 
+    else {
+        printf("%d is the greatest number.\n", c);
+    }
+
+    return 0;
+}
+
