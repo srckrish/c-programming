3dd4376d68bb334d48bfe10ef5de38d95ec8b672
srckrish <krish.dahal82@gmail.com>
2026-01-13 18:53:57 +0545

    print n natural numbers

---
+++ decision_and_loops/conditional_statements/print_n_natural_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num, i;
+    printf("Enter a number:\t");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
