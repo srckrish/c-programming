e8ca5d2c209de2b502d1d407581d42afe2218a11
srckrish <krish.dahal82@gmail.com>
2026-01-13 18:54:34 +0545

    Print n even numbers

---
+++ decision_and_loops/conditional_statements/print_n_even_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int num, i;
+    printf("Enter a number:\t");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
