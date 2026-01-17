64774e93e175449432f7f33a417c12a96f732e4f
srckrish <krish.dahal82@gmail.com>
2026-01-17 17:53:11 +0545

    Star Ascending

---
+++ 03_pattern_and_logic_building/star_patterns/star_ascending.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int count, i, j;
+
+    printf("Enter the number of counts:\n");
+    scanf("%d", &count);
+
+    for (i = 1; i <= count; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
