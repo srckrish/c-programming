d2319ce53f231d0b4fc095fcb0ebbf06a28e20dc
srckrish <krish.dahal82@gmail.com>
2026-01-20 17:15:01 +0545

    fix prime number

--- 02_decision_and_loops/conditional_statements/check_prime_number.c
+++ 02_decision_and_loops/conditional_statements/check_prime_number.c
@@ -1,21 +1,31 @@
 // WAP to print all factors of a number and check whether it is prime or not
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int number, i;
     int factorCount = 0;
 
     printf("Enter a number: ");
     scanf("%d", &number);
 
+    if (number < 2)
+    {
+        printf("%d is neither prime nor composite.\n", number);
+        return 0;
+    }
+
     printf("Factors of %d are: ", number);
 
-    for (i = 1; i * i <= number; i++) {
-        if (number % i == 0) {
+    for (i = 1; i * i <= number; i++)
+    {
+        if (number % i == 0)
+        {
             printf("%d ", i);
             factorCount++;
 
-            if (i != number / i) {
+            if (i != number / i)
+            {
                 printf("%d ", number / i);
                 factorCount++;
             }
@@ -24,12 +34,14 @@ int main() {
 
     printf("\n");
 
-    if (factorCount == 2) {
+    if (factorCount == 2)
+    {
         printf("%d is a prime number.\n", number);
-    } else {
-        printf("%d is a composite word.\n", number);
+    }
+    else
+    {
+        printf("%d is a composite number.\n", number);
     }
 
     return 0;
 }
-
