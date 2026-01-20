1d9e2f8a3f1768f699449affeacea67c55c0ddf5
srckrish <krish.dahal82@gmail.com>
2026-01-20 11:46:33 +0545

    fix factorial function code

--- 04_functions/factorial.c
+++ 04_functions/factorial.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+
 int factorial(int);
+
 int main()
 {
+    int num;
 
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int result = factorial(num);
+
+    if (result == -1)
+    {
+        printf("Invalid input. Factorial is defined only for non-negative integers.\n");
+    }
+    else
+    {
+        printf("Factorial = %d\n", result);
+    }
     return 0;
 }
 
-int factorial(int num)
+int factorial(int n)
 {
-    int i;
+    int i, fact = 1;
+
+    if (n < 0)
+    {
+        return -1;
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
 }
